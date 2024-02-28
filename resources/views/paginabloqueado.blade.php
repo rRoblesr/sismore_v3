492b0fd4b862db5b5c50d575044abff62779b6bb
rRoblesr <raroblesr@gmail.com>
2024-02-28 09:10:54 -0500

    xxx

---
+++ resources/views/paginabloqueado.blade.php
@@ -0,0 +1,5 @@
+<div class="row">
+    <a href="javascript:history.back()">
+        <img src="{{ asset('/') }}public/img/access_denied.jpg" alt="" style="width: 100%" height="100%">
+    </a>
+</div>
