cce00c149466f2a0a5f150a582ef385773a7e779
rRoblesr <raroblesr@gmail.com>
2024-09-16 16:22:55 -0500

    dfghdfhdfgh

--- app/Repositories/Educacion/CuboPacto2Repositorio.php
+++ app/Repositories/Educacion/CuboPacto2Repositorio.php
@@ -32,13 +32,10 @@ class CuboPacto2Repositorio
 
     public static function getEduPacto2tabla1($anio, $mes, $provincia, $distrito, $estado)
     {
-        $query = DB::table('edu_cubo_pacto02_local')->select(
-            'distrito',
-            DB::raw('count(local) as conteo'),
-        )->where(DB::raw('year(fecha_inscripcion)'), $anio);
+        $query = DB::table('edu_cubo_pacto02_local')->select('distrito', DB::raw('count(local) as conteo'))->where(DB::raw('year(fecha_inscripcion)'), $anio);
 
         if ($mes > 0)
-            $query = $query->where(DB::raw('month(fecha_inscripcion)'),'<=', $mes);
+            $query = $query->where(DB::raw('month(fecha_inscripcion)'), '<=', $mes);
         if ($provincia > 0)
             $query = $query->where('provincia_id', $provincia);
         if ($distrito > 0)
